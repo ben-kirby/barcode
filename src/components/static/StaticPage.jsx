b97147ba7e8a8ed936c628525bc09661777ece72
Ben Kirby <benkrby@gmail.com>
2019-03-01 13:05:56 -0800

    added component for static page

---
+++ src/components/static/StaticPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StaticPage(){
+  return(
+    <div>
+      <h1>static render</h1>
+    </div>
+  );
+}
+
+export default StaticPage;
---
+++ src/components/static/UPC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UPC = '040707478160';
+
+function ScannedUPC (){
+  return UPC;
+}
+
+export default ScannedUPC
