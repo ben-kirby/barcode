fdd1bf35b4effa00f7d215c1eac3411acec935f6
Ben Kirby <benkrby@gmail.com>
2019-03-01 10:40:38 -0800

    Added basic app files. Having trouble with a babel error.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Camera from './camera'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <Camera />
+      </div>
+    )
+  }
+}
+
+export default App
